2ed7ea16c4c2a963c9ca70b879794f91f4dc1dc5
Sodiq Olanrewaju Akanmu <sodiq.akanmu001@gmail.com>
2023-02-09 01:13:29 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,40 @@
-#!/usr/bin/node
+# 0x06. Star Wars API
+## Task
+    Write a script that prints all characters of a Star Wars movie:
 
-const request = require('request');
+    The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+    Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+    You must use the Star wars API
+    You must use the request module
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+    GitHub repository: alx-interview
+    Directory: 0x06-starwars_api
+    File: 0-starwars_characters.js
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
+## Example
+    alexa@ubuntu:~/0x06$ ./0-starwars_characters.js 3
+    Luke Skywalker
+    C-3PO
+    R2-D2
+    Darth Vader
+    Leia Organa
+    Obi-Wan Kenobi
+    Chewbacca
+    Han Solo
+    Jabba Desilijic Tiure
+    Wedge Antilles
+    Yoda
+    Palpatine
+    Boba Fett
+    Lando Calrissian
+    Ackbar
+    Mon Mothma
+    Arvel Crynyd
+    Wicket Systri Warrick
+    Nien Nunb
+    Bib Fortuna
+    alexa@ubuntu:~/0x06$ 
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
-};
-
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+## Resources
+- [swapi-api.hbtn.io](https://swapi-api.hbtn.io/)    
+- [request module](https://github.com/request/request)  
